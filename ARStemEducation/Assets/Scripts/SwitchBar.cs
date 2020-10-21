b3261e5bd4e545cc78c12a4fca1a6b1f0d1dc62c
Louis Tsui <louiss011371@hotmail.com>
2020-10-21 18:39:21 +0800

    Update -- bulb circuit diagram design / switch bar script

---
+++ ARStemEducation/Assets/Scripts/SwitchBar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBar : MonoBehaviour
+{
+    GameObject onOffButton;
+    public GameObject bulbObj;
+    GameObject bulbLight;
+    public Material lightOnMaterial;
+    public Material lightOffMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onOffButton = this.gameObject;
+        Debug.Log("SwtichStatus loaded");
+        bulbObj = GameObject.Find("Bulb");
+        bulbLight = bulbObj.gameObject.transform.GetChild(0).gameObject;
+    }
+    void Update()
+    {
+        SwitchController();
+    }
+
+    public void SwitchController()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("before w key down " + Quaternion.Euler(onOffButton.transform.rotation.x, onOffButton.transform.rotation.y, onOffButton.transform.rotation.z));
+            StatusOff();
+            Debug.Log("after w key down " + onOffButton.transform.localRotation.x);
+
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            Debug.Log("before s key down " + onOffButton.transform.rotation.x);
+            StatusOn();
+            Debug.Log("after s key down " + onOffButton.transform.rotation.x);
+        }
+    }
+
+    public void StatusOn()
+    {
+        if (onOffButton.transform.localRotation.x * 10 > 1.7)
+        {
+            onOffButton.transform.Rotate(-20, 0, 0);
+            bulbLight.GetComponent<MeshRenderer>().material = lightOnMaterial;
+        }
+    }
+
+    public void StatusOff()
+    {
+        if (onOffButton.transform.localRotation.x < 0.17)
+        {
+
+            onOffButton.transform.Rotate(20, 0, 0);
+
+            bulbLight.GetComponent<MeshRenderer>().material = lightOffMaterial;
+        }
+    }
+}
