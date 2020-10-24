33f0f360d97b01eb8f5133f3a87a420d39cb7e17
Louis Tsui <louiss011371@hotmail.com>
2020-10-24 17:00:48 +0800

    Update -- H2O AR -- null reference when object (clone) not done

---
+++ ARStemEducation/Assets/Scripts/ARChemical/ARElementRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARElementRotate : MonoBehaviour
+{
+   GameObject hTwoOBoj;
+    //OnCLickListener script;
+
+    private void Start()
+    {
+        hTwoOBoj = GameObject.Find("H2OAR");
+        //hTwoOBoj = this.gameObject;
+        Debug.Log(hTwoOBoj);
+       // script = new OnCLickListener();
+    }
+
+    public void RotateLeft()
+    {      
+        Debug.Log("RotateLeft");
+        hTwoOBoj.gameObject.transform.Rotate(0, -1, 0);
+       // script.TestPrint();
+    }
+
+    public void RotateRight()
+    {
+        Debug.Log("RotateRight");
+        hTwoOBoj.gameObject.transform.Rotate(0, 1, 0);
+    }
+}
