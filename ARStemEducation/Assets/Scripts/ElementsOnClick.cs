d26d6efa8f2d2c897aa4a9db3671735101a07054
Louis Tsui <louiss011371@hotmail.com>
2020-10-27 22:08:47 +0800

    Update -- simple virtual button function

---
+++ ARStemEducation/Assets/Scripts/ElementsOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ElementsOnClick : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject model, vButton;
+
+    private void Start()
+    {
+        model = GameObject.Find("H2O");
+        vButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        model.gameObject.transform.Rotate(0, 90, 0);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        model.gameObject.transform.Rotate(0, 0, 0);
+    }
+}
