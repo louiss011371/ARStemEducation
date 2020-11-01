755511420e41e456a32bc7639e85487100feb5d2
Louis Tsui <louiss011371@hotmail.com>
2020-11-01 16:10:23 +0800

    Add -- Drag object sample scene and logic

---
+++ ARStemEducation/Assets/Scripts/Test/TestCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+    private void OnCollisionEnter()
+    {
+        Debug.Log("two elements has collised");
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("trigger entered name : " + collider.name);
+    }
+}
