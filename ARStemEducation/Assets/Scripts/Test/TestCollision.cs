f41c7e401e5d8e91bc1d6f6082d9afb3151e1cb1
Louis Tsui <louiss011371@hotmail.com>
2020-11-05 22:05:41 +0800

    Update -- TestCollision script

--- ARStemEducation/Assets/Scripts/Test/TestCollision.cs
+++ ARStemEducation/Assets/Scripts/Test/TestCollision.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class TestCollision : MonoBehaviour
 {
+    public Animator anim;
+    private void Start()
+    {
+        //anim = GetComponent<Animator>();
+        anim.Play("None");
+    }
+    private void Update()
+    {
+       if(Input.GetKey("a"))
+        {
+            Debug.Log("input a was detected");
+            anim.Play("Burning");
+        }
+    }
     // Check two trigger objects are collided
     private void OnTriggerEnter(Collider collider)
     {
         Debug.Log("trigger entered name : " + collider.name);
         if(collider.name == "Fe" || collider.name == "S")
         {
-            Destroy(collider.gameObject);
+           // Destroy(collider.gameObject);
         }
     }
 }
