5800faa94ff09ac581691d09988630ede8377f83
Louis Tsui <louiss011371@hotmail.com>
2020-11-06 15:36:07 +0800

    Update -- TestScene Scene / TestCollision Script / FesSet Prefab

--- ARStemEducation/Assets/Scripts/Test/TestCollision.cs
+++ ARStemEducation/Assets/Scripts/Test/TestCollision.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class TestCollision : MonoBehaviour
 {
     public Animator anim;
+    public GameObject feObj, sObj;
+    public GameObject fesTextObj;
     private void Start()
     {
         //anim = GetComponent<Animator>();
         anim.Play("None");
+        fesTextObj.SetActive(false);
     }
     private void Update()
     {
        if(Input.GetKey("a"))
         {
-            Debug.Log("input a was detected");
             anim.Play("Burning");
+            fesTextObj.SetActive(true);
         }
+       if(sObj != null)
+        {
+            ChangeElement();
+        }
+        else
+        {
+            ChangeColor();
+        }
+    }
+
+    // if Fe element position is equal to S element
+    private void ChangeElement()
+    {
+        if (feObj.transform.localPosition == sObj.transform.localPosition)
+        {
+            Debug.Log("Fe and S merged");
+            Destroy(sObj);
+        }
+    }
+    private void ChangeColor()
+    {
+        anim.Play("ChangeColor");
+        Debug.Log("ChangeColor() play");
     }
     // Check two trigger objects are collided
     private void OnTriggerEnter(Collider collider)
