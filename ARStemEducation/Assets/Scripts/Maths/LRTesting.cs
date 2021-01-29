4a76f5bd2ff9ebd1c7b3694b4401a8ee256edae8
Louis Tsui <louiss011371@hotmail.com>
2021-01-29 14:27:11 +0800

    Add -- Maths Folder / LRTesting /LRLineController for add line on screen connect two points

---
+++ ARStemEducation/Assets/Scripts/Maths/LRTesting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LRTesting : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    [SerializeField] private LRLineController line;
+
+    // Use this for initialization
+    void Start()
+    {
+        line.SetUpLine(points);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
