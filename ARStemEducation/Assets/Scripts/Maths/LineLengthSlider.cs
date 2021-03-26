f2fb14fe379c55a0bbf9664cab7a796000b9bbaa
Louis Tsui <louiss011371@hotmail.com>
2021-03-26 13:18:10 +0800

    Add -- LineLengthSlide / StoreLineLength -- Add LineLength

---
+++ ARStemEducation/Assets/Scripts/Maths/LineLengthSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineLengthSlider : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.minValue = 1;
+        slider.maxValue = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("value of slider = " + slider.value);
+    }
+    public float getSliderValue()
+    {
+        return slider.value;
+    }
+    public void setSliderValue(float value)
+    {
+        this.slider.value = value;
+    }
+}
